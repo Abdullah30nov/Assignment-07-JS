355ee8fc02a84dedac5b157e88cb06b1edd617fb
Abdullah30nov <abdullahbinaslam64@gmail.comgit config --global user.email abdullahbinaslam64@gmail.com>
2024-03-25 01:50:44 -0700

    Math Methods done

---
+++ app.js
@@ -0,0 +1,58 @@
+                                   //QUESTION-NO-01
+// var usernumber = +prompt("Enter positive integer number...");
+
+// var round = Math.round(usernumber);
+// var floor = Math.floor(usernumber);
+// var ceil = Math.ceil(usernumber);
+// document.write("Number: ",usernumber + "<br>" + "Round: " + round + "<br>" + "Ceil: " + ceil + "<br>" + "Floor: " + floor);
+
+
+                                   //QUESTION-NO-02
+// var usernumber = +prompt("Enter negative floating point...");
+
+// var round = Math.round(usernumber);
+// var floor = Math.floor(usernumber);
+// var ceil = Math.ceil(usernumber);
+// document.write("Number: ",usernumber + "<br>" + "Round: " + round + "<br>" + "Ceil: " + ceil + "<br>" + "Floor: " + floor);
+
+                                         //QUESTION-NO-03
+// let num = parseFloat(prompt("Enter a number:"));
+// function absoluteValue(num) {
+//     if (num < 0) {
+//         return -num;
+//     } else {
+//         return num;
+//     }
+// }
+// document.write("The absolute value of ", num, " is ", absoluteValue (num));
+
+                                     //QUESTION-NO-04
+// let diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write("Roll the dice: " + diceValue)
+
+                                       //QUESTION-NO-05
+// var heads = prompt("Enter head user name...")
+// var tail = prompt("Enter tail user name...")
+// var toss = Math.floor(Math.random()*2);
+// if(toss === 0){
+//     document.write(heads + " win the Toss.")
+// }
+// else{
+//     document.write(tail+" win the Toss.")
+// }
+
+                                       //QUESTION-NO-06
+// var number = Math.floor(Math.random()*100);
+// document.write("The random number is: "+number)
+
+
+                                       //QUESTION-NO-08
+// var secretnum = 8;
+// var userinput = +prompt("Guess the secret number 1-10...");
+// if(userinput === secretnum){
+//     document.write("Congratulation you'r correct.")
+// }
+// else{
+//     document.write("Try again.")
+// }
+
